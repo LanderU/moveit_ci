5470555771a781a3d00cd33feb1e3799bb4841a9
LanderU <lander.usategui@gmail.com>
2019-02-24 20:58:23 +0100

    Ament instead of catkin

--- check_ament_lint.sh
+++ check_ament_lint.sh
@@ -5,13 +5,13 @@
 
 travis_fold start check.catkin_lint "Checking for issues reported by ament_lint"
 
-travis_run apt-get -qq install -y python-catkin-lint
-travis_run --title "Running catkin_lint in repository source: $CI_SOURCE_PATH" \
+travis_run apt-get -qq install -y ros-$ROS_DISTRO-ament-lint-common
+travis_run --title "Running ament_lint in repository source: $CI_SOURCE_PATH" \
     catkin_lint $CI_SOURCE_PATH
 result=$?
 
 # Finish fold before printing result summary
-travis_fold end check.catkin_lint
+travis_fold end check.ament_lint
 
 if [ $result -eq 0 ] ; then
   echo -e $(colorize GREEN "No catkin_lint issues reported.")
